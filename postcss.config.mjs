b71258d7964e1c62f62b05dcf16e33061ad13eea
Hyuuuka <86443416+hyuuuka@users.noreply.github.com>
2025-04-15 19:06:20 +0100

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+/** @type {import('postcss').Config} */
+module.exports = {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/postcss": {},
   },
-};
-
-export default config;
+}
